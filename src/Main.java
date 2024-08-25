59a255a96004b6d1397da42756c1cb5bdf0e11d7
rahul sahu <rahul@beeyond.agency>
2024-08-25 10:58:01 +0530

    first program

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Good morning");
+
+
+    }
+    //Right half Pyramid
+//    public static void main(String[] args) {
+//        System.out.println("*");
+//        System.out.println("* *");
+//        System.out.println("* * *");
+//        System.out.println("* * * *");
+//        System.out.println("* * * * *");
+//
+//    }
+
+    // reverse right half Pyramid
+//    public static void main(String[] args) {
+//        System.out.println("* * * * *");
+//        System.out.println("* * * *");
+//        System.out.println("* * *");
+//        System.out.println("* * ");
+//        System.out.println("* ");
+//
+//    }
+
+    //left half Pyramid
+
+//    public static void main(String[] args) {
+//        System.out.println("        *");
+//        System.out.println("      * *");
+//        System.out.println("    * * *");
+//        System.out.println("  * * * *");
+//        System.out.println("* * * * * ");
+//
+//    }
+
+}
+
+
+
+
